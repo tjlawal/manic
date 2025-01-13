9ce320dc604167d40a41d4765a29b83a33d395bb
Tijani Lawal <tijani@tlawal.org>
2025-01-13 19:40:46 +0000

    Build file now embeds manifest files when compiled on MSVC, generated for fatal crashes in to the executable. clang was already present, forgot to add MSVC as a result of neglect.

--- build.bat
+++ build.bat
@@ -15,20 +15,20 @@ if "%clang%"=="1"       set msvc=0     && echo [clang compile]
 
 :: ------------ Unpack Command Line Build Arguments -------------------
 set auto_compile_flags=
-if "%asan%"=="1" set auto_compile_flags=%auto_compile_flags% -fsanitize=address && echo [asan enabled]
+if "%asan%"=="1" 				set auto_compile_flags=%auto_compile_flags% -fsanitize=address && echo [asan enabled]
 
 :: ------------ MSVC --------------------------------------------------
 set cl_common=     /I..\src\ /nologo /FC /Z7 /W1
 set cl_debug=      call cl /Od /DBUILD_DEBUG=1 %cl_common% %auto_compile_flags%
 set cl_release=    call cl /O2 /DBUILD_DEBUG=0 %cl_common% %auto_compile_flags%
-set cl_link=       /link /INCREMENTAL:NO
+set cl_link=       /link /MANIFEST:EMBED /INCREMENTAL:NO /PDBALTPATH:%%%%_PDB%%%%
 set cl_out=        /out:
 
 :: ------------ Clang -------------------------------------------------
 set clang_common=   -I..\src\ -Wall -gcodeview -fdiagnostics-absolute-paths -std=c99 -Wno-initializer-overrides -Wno-unused-function -Wno-missing-braces -Wno-unused-variable 
 set clang_debug=    call clang -g -O0 -DBUILD_DEBUG=1 %clang_common% %auto_compile_flags%
 set clang_release=  call clang -g -O2 -DBUILD_DEBUG=0 %clang_common% %auto_compile_flags%
-set clang_link=     -fuse-ld=lld -Xlinker /MANIFEST:EMBED
+set clang_link=     -fuse-ld=lld -Xlinker /MANIFEST:EMBED -Xlinker /pdbaltpath:%%%%_PDB%%%%
 set clang_out=      -o
 
 :: ------------ Per-Build Setting -------------------------------------
