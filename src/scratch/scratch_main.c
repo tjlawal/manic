168ecd030e5e443f75a0e44952017ea3de059d7e
Tijani <tijani@tlawal.org>
2025-01-13 19:13:16 +0000

    fatal crash now tested and works as expected on the console too in win32.

--- src/scratch/scratch_main.c
+++ src/scratch/scratch_main.c
@@ -11,37 +11,17 @@
 #include "os/os_inc.c"
 // clang-format on
 
+#include <shlwapi.h>
+
 void entry_point() {
+  // Test exception on the terminal
+  // int *ptr = NULL;
+  //*ptr = 5;
 
-  // ArenaParams params = {.reserve_size = MB(40), .commit_size = KB(64), .flags
-  // = 0, .optional_preallocated_buffer = 0};
-  Arena *temp_memory = arena_alloc();
+  // printf("%d", ptr);
 
-  typedef struct User {
-    string8 name;
-    int age;
-  } User;
+  int z = 0;
+  int r = 1 / z;
 
-  Temp scratch = temp_begin(temp_memory);
-
-  // Allocate array of users
-  User *users = push_array(scratch.arena, User, 3);
-
-  // Initialize users
-  users[0].name = push_str8_copy(scratch.arena, str8_lit("Alice"));
-  users[0].age = 25;
-
-  users[1].name = push_str8_copy(scratch.arena, str8_lit("Bob"));
-  users[1].age = 30;
-
-  users[2].name = push_str8_copy(scratch.arena, str8_lit("Charlie"));
-  users[2].age = 35;
-
-  // Print all users
-  for (int i = 0; i < 3; i++) {
-    printf("User: %.*s, Age: %d\n", (int)users[i].name.size, users[i].name.str, users[i].age);
-  }
-
-  temp_end(scratch); // Frees all allocated memory
-  arena_release(temp_memory);
+  printf("%d", r);
 }
