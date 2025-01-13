2e4587ffb9846ce77d3de5b371d11e70cf6653d4
Tijani <tijani@tlawal.org>
2025-01-13 19:20:16 +0000

    removing old ways of camera projection.

--- src/camera/camera_inc.h
+++
@@ -1,8 +0,0 @@
-// Copyright Frost Gorilla, Inc. All Rights Reserved.
-
-#ifndef CAMERA_INC_H
-#define CAMERA_INC_H
-
-#include "core/camera_core.h"
-
-#endif // CAMERA_INC_H
--- src/camera/projection/orthographic.c
+++
@@ -1,7 +0,0 @@
-// Copyright Frost Gorilla, Inc. All Rights Reserved.
-
-// Orthographic Projection - convert 3D points into a 2D projected point
-Vec2F32 orthographic_projection(Vec3F32 points) {
-  Vec2F32 projected_point = {.x = (field_of_view_factor * points.x), .y = (field_of_view_factor * points.y)};
-  return projected_point;
-}
