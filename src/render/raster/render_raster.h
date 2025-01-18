f111822b1fcd4f913ff9092ebf8191cf822c0cf7
Tijani <tijani@tlawal.org>
2025-01-18 16:06:50 +0000

    Remove draw calls from render layer.

--- src/render/raster/render_raster.h
+++ src/render/raster/render_raster.h
@@ -15,35 +15,8 @@ struct RenderBackBuffer {
   u32 *colour_buffer;
 };
 
-// clang-format off
-
 internal void r_resize_buffer(Arena *arena, RenderBackBuffer *buffer, s32 width, s32 height);
 internal void r_copy_buffer_to_window(HDC device_context, RenderBackBuffer *buffer);
-
-internal void r_draw_pixel(RenderBackBuffer *buffer, s32 x, s32 y, u32 colour);
 internal void r_clear_colour_buffer(RenderBackBuffer *buffer, u32 colour);
-
-internal void r_draw_line_dda(RenderBackBuffer *buffer, s32 x0, s32 y0, s32 x1, s32 y1, u32 colour);
-
-internal void r_draw_triangle(RenderBackBuffer *buffer, s32 x0, s32 y0, s32 x1, s32 y1, s32 x2, s32 y2, u32 colour);
-
-internal void r_draw_filled_triangle(RenderBackBuffer *buffer, s32 x0, s32 y0, s32 x1, s32 y1, s32 x2, s32 y2,
-                                     u32 colour);
-
-internal void r_draw_textured_triangle(RenderBackBuffer *buffer, s32 x0, s32 y0, f32 u0, f32 v0, s32 x1, s32 y1, f32 u1,
-                                       f32 v1, s32 x2, s32 y2, f32 u2, f32 v2, u32 *texture);
-
-internal void r_draw_fill_flat_bottom_triangle(RenderBackBuffer *buffer, s32 x0, s32 y0, s32 x1, s32 y1, s32 x2, s32 y2,
-                                               u32 colour);
-
-internal void r_draw_fill_flat_top_triangle(RenderBackBuffer *buffer, s32 x0, s32 y0, s32 x1, s32 y1, s32 x2, s32 y2,
-                                            u32 colour);
-
-internal void r_draw_texel(RenderBackBuffer *buffer, s32 x, s32 y, Vec2F32 a, Vec2F32 b, Vec2F32 c, f32 u0, f32 v0, f32 u1, f32 v1, f32 u2,
-                           f32 v2, u32 *texture);
-
-void r_draw_dots(RenderBackBuffer *buffer, u32 colour);
-void r_draw_grid(RenderBackBuffer *buffer, u32 colour);
-void r_draw_rect(RenderBackBuffer *buffer, s32 x, s32 y, s32 width, s32 height, u32 colour);
 
 #endif // RENDER_RASTER_H
