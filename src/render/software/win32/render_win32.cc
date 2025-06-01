a7b4380c7a6dc7611a9b054665292d13b40debbb
Tijani <tijani@tlawal.org>
2025-06-01 00:51:08 +0000

    Related to last commit.
    git-svn-id: https://svn.tlawal.org/manic/branches/rewrite@168 fb74ff46-d223-4cf3-9db4-1b2dc0db53d6

--- src/render/software/win32/render_win32.cc
+++ src/render/software/win32/render_win32.cc
@@ -3,55 +3,52 @@ using namespace Starlight::Foundation;
 
 namespace Starlight {
 	namespace Render {
-		
 
-		// TODO(tijani): this function is too os specific.
 		void r_resize_buffer(Arena *arena, Renderer *buffer, s32 width, s32 height) {
-			buffer->software_framebuffer.width = width;
-			buffer->software_framebuffer.height = height;
-			buffer->software_framebuffer.bytes_per_pixel = 4;
-			buffer->software_framebuffer.win32_bitmapinfo.bmiHeader.biSize = sizeof(buffer->software_framebuffer.win32_bitmapinfo.bmiHeader);
-			buffer->software_framebuffer.win32_bitmapinfo.bmiHeader.biWidth = buffer->software_framebuffer.width;
-			buffer->software_framebuffer.win32_bitmapinfo.bmiHeader.biHeight = buffer->software_framebuffer.height; // This would be negative when going top-down
-			buffer->software_framebuffer.win32_bitmapinfo.bmiHeader.biPlanes = 1;
-			buffer->software_framebuffer.win32_bitmapinfo.bmiHeader.biBitCount = 32;
-			buffer->software_framebuffer.win32_bitmapinfo.bmiHeader.biCompression = BI_RGB;
+			buffer->sw.width = width;
+			buffer->sw.height = height;
+			buffer->sw.bytes_per_pixel = 4;
+			buffer->sw.win32_bitmapinfo.bmiHeader.biSize = sizeof(buffer->sw.win32_bitmapinfo.bmiHeader);
+			buffer->sw.win32_bitmapinfo.bmiHeader.biWidth = buffer->sw.width;
+			buffer->sw.win32_bitmapinfo.bmiHeader.biHeight = buffer->sw.height; // This would be negative when going top-down
+			buffer->sw.win32_bitmapinfo.bmiHeader.biPlanes = 1;
+			buffer->sw.win32_bitmapinfo.bmiHeader.biBitCount = 32;
+			buffer->sw.win32_bitmapinfo.bmiHeader.biCompression = BI_RGB;
 
-			s32 bitmap_memory_size = (buffer->software_framebuffer.width * buffer->software_framebuffer.height) * buffer->software_framebuffer.bytes_per_pixel;
+			s32 pixel_count = buffer->sw.width * buffer->sw.height;
+			s32 colour_buffer_size = pixel_count * sizeof(u32); // 4 bytes per pixel
+			s32 z_buffer_size = pixel_count * sizeof(f32); // 4 bytes per depth value
+			s32 total_buffer_size = colour_buffer_size + z_buffer_size;
 
-			//buffer->memory = push_array(arena, u8, bitmap_memory_size);
-			buffer->software_framebuffer.memory = arena_push<u8>(arena, bitmap_memory_size);
-			buffer->software_framebuffer.colour_buffer = (u32 *)buffer->software_framebuffer.memory;
-
-			// NOTE(tijani): is this a good way to do this?
-			f32 z_buffer_memory_size = (buffer->software_framebuffer.width * buffer->software_framebuffer.height) * buffer->software_framebuffer.bytes_per_pixel;
-			//f32 *z_buffer_memory = push_array(arena, f32, z_buffer_memory_size);
-			f32* z_buffer_memory = arena_push<f32>(arena, z_buffer_memory_size);
-
-			buffer->software_framebuffer.z_buffer = z_buffer_memory;
+			// One allocation
+			buffer->sw.memory_buffer = arena_push<u8>(arena, total_buffer_size);
+			u8* mem_block = static_cast<u8*>(buffer->sw.memory_buffer);
+			// Concerned parties get their share!
+			buffer->sw.colour_buffer = reinterpret_cast<u32*>(mem_block);
+			// Offset cause its stored contiguously alongside colour_buffer!
+			buffer->sw.z_buffer = reinterpret_cast<f32*>(mem_block + colour_buffer_size);
 		}
 
-		// TODO(tijani): This is too OS specific.
 		void r_copy_buffer_to_window(HDC device_context, Renderer *buffer) {
-			StretchDIBits(device_context, 0, 0, buffer->software_framebuffer.width, buffer->software_framebuffer.height, 0, 0, 
-										buffer->software_framebuffer.width, buffer->software_framebuffer.height, buffer->software_framebuffer.memory, 
-										&buffer->software_framebuffer.win32_bitmapinfo, DIB_RGB_COLORS, SRCCOPY);
+			StretchDIBits(device_context, 0, 0, buffer->sw.width, buffer->sw.height, 0, 0, 
+										buffer->sw.width, buffer->sw.height, buffer->sw.colour_buffer, 
+										&buffer->sw.win32_bitmapinfo, DIB_RGB_COLORS, SRCCOPY);
 		}
 
-		// TODO(tijani): This is stupid and could be so much faster.
+		// REVISE: Could this be faster?
 		void r_clear_colour_buffer(Renderer *buffer, u32 colour) {
-			for (s32 y = 0; y < buffer->software_framebuffer.height; y++) {
-				for (s32 x = 0; x < buffer->software_framebuffer.width; x++) {
-					buffer->software_framebuffer.colour_buffer[(buffer->software_framebuffer.width * y) + x] = colour;
+			for (s32 y = 0; y < buffer->sw.height; y++) {
+				for (s32 x = 0; x < buffer->sw.width; x++) {
+					buffer->sw.colour_buffer[(buffer->sw.width * y) + x] = colour;
 				}
 			}
 		}
 
-		// TODO(tijani): This is stupid and could be so much faster.
+		// REVISE: Could this be faster?
 		void r_clear_z_buffer(Renderer *buffer) {
-			for (s32 y = 0; y < buffer->software_framebuffer.height; ++y) {
-				for (s32 x = 0; x < buffer->software_framebuffer.width; ++x) {
-					buffer->software_framebuffer.z_buffer[(buffer->software_framebuffer.width * y) + x] = 1.0f;
+			for (s32 y = 0; y < buffer->sw.height; ++y) {
+				for (s32 x = 0; x < buffer->sw.width; ++x) {
+					buffer->sw.z_buffer[(buffer->sw.width * y) + x] = 1.0f;
 				}
 			}
 		}
