e9803cbbe906f473ab83bfb3c73f8a2817492258
Tijani <tijani@tlawal.org>
2025-06-20 22:16:33 +0000

    Add tracy profiler support. Tracy doesn't work with manic Clang C++11 flags, weird don't know why, but it works with MSVC.
    git-svn-id: https://svn.tlawal.org/manic/branches/rewrite@206 fb74ff46-d223-4cf3-9db4-1b2dc0db53d6

--- src/third_party/third_party.h
+++ src/third_party/third_party.h
@@ -15,6 +15,11 @@
 	#if PROFILER_SUPERLUMINAL && OS_WINDOWS
 		#include "Superluminal/include/PerformanceAPI.h"
 		#pragma comment(lib, "../src/third_party/Superluminal/libs/PerformanceAPI_MT")
+	#elif PROFILER_TRACY && OS_WINDOWS
+		#define TRACY_ENABLE
+		#include "tracy/include/tracy/Tracy.hpp"
+		#include "tracy/include/TracyClient.cpp"
+		#pragma comment(lib, "../src/third_party/tracy/libs/TracyClient")
 	#else 
 		#error "Profiler not recognized, are you using the correct profiler flag?"
 	#endif
